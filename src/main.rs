af54b361e53490eb3c21589a5f4beb239dbd43b3
Luigi311 <git@luigi311.com>
2023-01-26 10:52:04 -0700

    Basic incrementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::app::App;
 use bevy::prelude::*;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
 use serde_json::Value;
 
 #[derive(Component)]
@@ -34,6 +35,9 @@ struct Price(f64);
 
 #[derive(Component)]
 struct Rewards(f64);
+
+#[derive(Component)]
+struct Worth(f64);
 
 #[derive(Component)]
 struct Subscription(bool);
@@ -71,6 +75,7 @@ fn read_games_json() -> serde_json::Value {
             "difficulty_growth": 0.0,
             "difficulty_ticks": 0.0,
             "rewards": 1.0,
+            "worth": 0.0001,
             "workers": 0,
             "workers_cap": 0,
             "currency": 0.0,
@@ -97,6 +102,7 @@ fn add_games(mut commands: Commands) {
             DifficultyGrowth(game_data["difficulty_growth"].as_f64().unwrap()),
             DifficultyTicks(game_data["difficulty_ticks"].as_f64().unwrap()),
             Rewards(game_data["rewards"].as_f64().unwrap()),
+            Worth(game_data["worth"].as_f64().unwrap()),
             Workers(game_data["workers"].as_u64().unwrap()),
             WorkersCap(game_data["workers_cap"].as_u64().unwrap()),
             Currency(game_data["currency"].as_f64().unwrap()),
@@ -111,8 +117,7 @@ fn add_games(mut commands: Commands) {
 #[derive(Resource)]
 struct PrintTimer(Timer);
 
-// Query all games and print them
-fn print_all(
+fn print_player(
     time: Res<Time>,
     mut timer: ResMut<PrintTimer>,
     query: Query<(
@@ -122,15 +127,9 @@ fn print_all(
         &DifficultyBase,
         &DifficultyGrowth,
         &DifficultyTicks,
-        Option<&Game>,
-        Option<&Name>,
-        Option<&Price>,
-        Option<&Rewards>,
-        Option<&Subscription>,
-        Option<&Population>,
-        Option<&Player>,
-        Option<&RebornPoints>,
-        Option<&IdleTokens>,
+        &RebornPoints,
+        &IdleTokens,
+        With<Player>,
     )>,
 ) {
     if timer.0.tick(time.delta()).just_finished() {
@@ -141,46 +140,78 @@ fn print_all(
             difficulty_base,
             difficulty_growth,
             difficulty_ticks,
-            game,
-            name,
-            price,
-            rewards,
-            subscription,
-            population,
-            player,
             reborn_points,
             idle_tokens,
+            _
         ) in query.iter()
         {
-            if let Some(_game) = game {
-                println!("{}", name.unwrap().0);
-                println!("\tPrice: \t\t{}", price.unwrap().0);
-                println!("\tSubscription: {}", subscription.unwrap().0);
-                println!("\tDiff_base: \t{}", difficulty_base.0);
-                println!("\tDiff_grow: \t{}", difficulty_growth.0);
-                println!("\tDiff_ticks: \t{}", difficulty_ticks.0);
-                println!("\tRewards: \t{}", rewards.unwrap().0);
-                println!("\tWorkers: \t{}", workers.0);
-                println!("\tWorkers_cap: \t{}", workers_cap.0);
-                println!("\tCurrency: \t{}", currency.0);
-                println!("\tPopulation: \t{}", population.unwrap().0);
-            } else if let Some(_player) = player {
-                println!("Player");
-                println!("\tRebornPoints: \t{}", reborn_points.unwrap().0);
-                println!("\tIdleTokens: \t{}", idle_tokens.unwrap().0);
-                println!("\tCurrency: \t{}", currency.0);
-                println!("\tWorkers: \t{}", workers.0);
-                println!("\tWorkers_cap: \t{}", workers_cap.0);
-                println!("\tDiff_base: \t{}", difficulty_base.0);
-                println!("\tDiff_grow: \t{}", difficulty_growth.0);
-                println!("\tDiff_ticks: \t{}", difficulty_ticks.0);
-            } else {
-                println!("Unknown entity");
-            }
+            println!("Player");
+            println!("\tCurrency: \t{}", currency.0);
+            println!("\tWorkers: \t{}", workers.0);
+            println!("\tWorkers_cap: \t{}", workers_cap.0);
+            println!("\tDiff_base: \t{}", difficulty_base.0);
+            println!("\tDiff_grow: \t{}", difficulty_growth.0);
+            println!("\tDiff_ticks: \t{}", difficulty_ticks.0);
+            println!("\tReborn_points: \t{}", reborn_points.0);
+            println!("\tIdle_tokens: \t{}", idle_tokens.0);
         }
     }
 }
 
+// Query all games and print them
+fn print_games(
+    time: Res<Time>,
+    mut timer: ResMut<PrintTimer>,
+    query: Query<(
+        &Currency,
+        &Workers,
+        &WorkersCap,
+        &DifficultyBase,
+        &DifficultyGrowth,
+        &DifficultyTicks,
+        &Name,
+        &Price,
+        &Rewards,
+        &Worth,
+        &Subscription,
+        &Population,
+        With<Game>,
+    )>,
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for (
+            currency,
+            workers,
+            workers_cap,
+            difficulty_base,
+            difficulty_growth,
+            difficulty_ticks,
+            name,
+            price,
+            rewards,
+            worth,
+            subscription,
+            population,
+            _
+        ) in query.iter()
+        {
+            println!("{}", name.0);
+            println!("\tPrice: \t\t{}", price.0);
+            println!("\tSubscription: {}", subscription.0);
+            println!("\tDiff_base: \t{}", difficulty_base.0);
+            println!("\tDiff_grow: \t{}", difficulty_growth.0);
+            println!("\tDiff_ticks: \t{}", difficulty_ticks.0);
+            println!("\tRewards: \t{}", rewards.0);
+            println!("\tWorth: \t\t{}", worth.0);
+            println!("\tWorkers: \t{}", workers.0);
+            println!("\tWorkers_cap: \t{}", workers_cap.0);
+            println!("\tCurrency: \t{:.2}", currency.0);
+            println!("\tPopulation: \t{}", population.0);
+        }
+    }
+}
+
+// Increase difficulty ticks
 fn difficulty_tick(
     time: Res<Time>,
     mut query: Query<(
@@ -207,11 +238,15 @@ fn difficulty_tick(
     }
 }
 
-// Assign worker to game based on name
-fn _assign_worker(mut query: Query<(&Name, &mut Workers)>, game_name: String) {
-    for (name, mut workers) in query.iter_mut() {
-        if name.0 == game_name {
-            workers.0 += 1;
+// Increase currency
+fn currency_tick(
+    time: Res<Time>,
+    mut query: Query<(&mut Currency, &Workers, &Rewards, With<Game>)>,
+) {
+    for (mut currency, workers, rewards, _) in query.iter_mut() {
+        if workers.0 > 0 {
+            let tick = time.delta_seconds_f64() * (workers.0 as f64);        
+            currency.0 += tick * rewards.0;
         }
     }
 }
@@ -219,10 +254,100 @@ fn _assign_worker(mut query: Query<(&Name, &mut Workers)>, game_name: String) {
 fn main() {
     App::new()
         .insert_resource(PrintTimer(Timer::from_seconds(3.0, TimerMode::Repeating)))
+        .init_resource::<UiState>()
         .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
         .add_startup_system(create_player)
         .add_startup_system(add_games)
         .add_system(difficulty_tick)
-        .add_system(print_all)
+        .add_system(currency_tick)
+        .add_system(print_player)
+        .add_system(print_games)
+        .add_system(ui_system)
         .run();
+}
+
+fn number_format(number: f64) -> String {
+    if number < 1000.0 {
+        format!("{:.2}", number)
+    } else if number < 1000000.0 {
+        format!("{:.2}K", number / 1000.0)
+    } else if number < 1000000000.0 {
+        format!("{:.2}M", number / 1000000.0)
+    } else if number < 1000000000000.0 {
+        format!("{:.2}B", number / 1000000000.0)
+    } else if number < 1000000000000000.0 {
+        format!("{:.2}T", number / 1000000000000.0)
+    } else {
+        format!("{:.2}Q", number / 1000000000000000.0)
+    }
+}
+
+#[derive(Default, Resource)]
+struct UiState {
+    workers_to_add: i64
+}
+
+fn ui_system(
+    mut egui_context: ResMut<EguiContext>,
+    mut ui_state: ResMut<UiState>,
+    mut query: Query<(
+        &Name,
+        &mut Workers,
+        &WorkersCap,
+        &Currency,
+    )>
+) {
+    egui::CentralPanel::default().show(egui_context.ctx_mut(), |ui| {        
+        ui.horizontal(|ui| {
+            // Show workers to add
+            ui.label(format!("Workers to add: "));
+            if ui.button("1").on_hover_text("Set workers to add to 1").clicked() {
+                ui_state.workers_to_add = 1;
+            };
+            if ui.button("2X").on_hover_text("2x workers to add").clicked() {
+                ui_state.workers_to_add *= 2;
+            };
+            if ui.button("X/2").on_hover_text("x/2 workers to add").clicked() {
+                ui_state.workers_to_add /= 2;
+            };
+            if ui.button("-X").on_hover_text("Inverse count").clicked() {
+                ui_state.workers_to_add *= -1;
+            };
+            
+
+            ui.label(format!("{}", number_format(ui_state.workers_to_add as f64)));
+        });
+
+        // For each game,  add button to add/remove X workers, buttons to change x to 1, 2, X2 to add/remove, and show current workers
+        for (name, mut workers, workers_cap, currency) in query.iter_mut() {
+            ui.horizontal(|ui| {
+                
+                ui.label(&name.0);
+                
+                if ui.button("Add workers").on_hover_text("Add workers").clicked() {
+                    if ui_state.workers_to_add > 0 {
+                        workers.0 += ui_state.workers_to_add as u64;
+                    } else {
+                        if (ui_state.workers_to_add *-1) as u64 <= workers.0 {
+                            workers.0 -= (ui_state.workers_to_add *-1) as u64;
+                        } else {
+                            workers.0 = 0;
+                        }
+                    }
+                };
+
+                if ui.button("Reset workers").on_hover_text("Reset workers").clicked() {
+                    workers.0 = 0;
+                };
+
+                ui.horizontal(|ui| {
+                    ui.label(format!("Workers: {}", number_format(workers.0 as f64)));
+                    ui.label(format!("Workers cap: {}", number_format(workers_cap.0 as f64)));
+                    ui.label(format!("Currency: {}", number_format(currency.0)));
+
+                });
+            });
+        }
+    });
 }
